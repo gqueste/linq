f62f9dd0818e4a9e0302ee7d2b140f6dd268f1e4
gqueste <gabriel.queste@gmail.com>
2015-11-16 15:42:42 +0100

    Création d'un nouveau salon

--- src/app/components/creationSalon.js
+++ src/app/components/creationSalon.js
@@ -1,24 +1,65 @@
 var React = require('react');
+var Env = require('../../config/env');
+var Firebase = require("firebase");
 
 var CreationSalon = React.createClass({
-    makeid: function() {
-        var idSize = 5;
-        var text = "";
-        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-        for( var i=0; i < idSize; i++ )
-            text += possible.charAt(Math.floor(Math.random() * possible.length));
-        return text;
+    getInitialState: function(){
+        return {
+            idRoomCreated: -1,
+            afficherLien: false
+        }
+    },
+    insertRoomCallback: function(error){
+        if(error){
+            console.log(error);
+        }
+        else{
+            console.log("ok " + this.state.idRoomCreated);
+            this.setState({afficherLien: true});
+        }
+    },
+    insertRoom: function(ref){
+        var roomsRef = ref.child("rooms");
+        var refCreated = roomsRef.push({room:{id:'newRoom'}}, this.insertRoomCallback);
+        this.setState({idRoomCreated: refCreated.key()});
+    },
+    connectionCallback: function(error, authData, ref){
+        if (error) {
+            console.log("Authentication Failed!", error);
+        } else {
+            console.log("Authenticated successfully with payload:", authData);
+            this.insertRoom(ref);
+        }
     },
     createRoom: function(){
-        var roomId = this.makeid();
-        alert(roomId);
+        var env = new Env();
+        var ref = new Firebase(env.getFirebaseURL());
+        var callback = this.connectionCallback;
+        ref.authWithCustomToken(env.getFirebaseKey(), function(error, authData){
+            callback(error, authData, ref);
+        });
+    },
+    afficherLien: function(){
+        if(this.state.afficherLien){
+            return (
+                <div>
+                    <form>
+                        <div className="row">
+                            <input type="text" className="form-control" readOnly value={this.state.idRoomCreated} />
+                        </div>
+                    </form>
+                </div>
+            );
+        }
     },
     render: function() {
         return (
             /*jshint ignore:start */
-            <div className="row atEase">
-                <button type="button" className="row btn btn-success" onClick={this.createRoom}>Créer un salon</button>
+            <div>
+                <div className="row atEase">
+                    <button type="button" className="row btn btn-success" onClick={this.createRoom}>Créer un salon</button>
+                </div>
+                {this.afficherLien()}
             </div>
             /*jshint ignore:end */
         );
